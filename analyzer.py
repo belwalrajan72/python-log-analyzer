3b23fb0f869a4b85161fbe5c0ba31cb1daf7e05b
rajan_belwal <belwalrajan72@gmail.com>
2026-01-11 14:37:11 +0530

    Add input validation and robust log level handling

--- analyzer.py
+++ analyzer.py
@@ -1,29 +1,51 @@
 
 
 def read_logs(filepath):
-     with  open(filepath) as file :
-         text = file.read()
-         split = text.split()
-         return  list(split)
-     
-lines = read_logs("sample.log")
+     try :
+        with  open(filepath) as file :
+          text = file.read()
+          split = text.split()
+          
+     except Exception as e :
+        split = e 
+     return split
 
+
+
+test_list = ["INFO",  "User",  "logged", "in",
+"ERROR", "Database", "timeout",
+"INFO", "User", "logged", "out",
+"ERROR", "API", "failed",
+"ERROR", "Database", "timeout",
+"Info", "this", "is", "a", "new" "line" ]  
+
+# lines = read_logs("C:/Users/belwa/OneDrive/Desktop/python_log_analyzer/python-log-analyzer/sample.log")
+lines = test_list
 def count_log_levels(lines):
      dictionary = {}
-     for i in lines :
-         if i == "INFO" or i =="ERROR":
-          dictionary[i] = dictionary.get(i,0) +1 
+     if isinstance(lines, list) and lines != None :
+      for i in lines :
+          if i == "INFO" or i =="ERROR" or  i == "Info":
+           dictionary[i] = dictionary.get(i,0) +1 
 
-         else :
-            dictionary["other"] = dictionary.get("other",0) +1
+          else :
+             dictionary["other"] = dictionary.get("other",0) +1
+     
+     else :
+           dictionary
      return dictionary
+     
 
 count  = count_log_levels(lines)
 
 def print_reports(counts):
-     for word, frequency in counts.items():
+     if isinstance(count, dict) and len(count) != 0 :
+      for word, frequency in counts.items():
            print(f"{word}:  {frequency}")
-     
+     else :
+      print(f"The dictionary is empty")
+
+
 
 print_reports(count)
 
